5359ea4d9d56ce0ad3ec6f8a1d0bea0667b2e4ba
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2021-06-26 19:34:21 -0500

    updated compile string

--- cmp.bat
+++ cmp.bat
@@ -1,1 +1,3 @@
-g++ generator.cpp generator.h rnd.cpp rnd.h utils.cpp utils.h point.cpp point.h planet.cpp planet.h partition.cpp partition.h triangulation.cpp triangulation.h regionMapping.cpp regionMapping.h -o gen -IC:\codeProjects\nationGenerator\SFML-2.5.1\include -LC:\codeProjects\nationGenerator\SFML-2.5.1\lib -DSFML_STATIC -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32
+cls
+g++ -std=c++17 generator.cpp generator.h rnd.cpp rnd.h utils.cpp utils.h entity.cpp entity.h entityHandler.cpp entityHandler.h textureTable.cpp textureTable.h point.cpp point.h planet.cpp planet.h unit.cpp unit.h partition.cpp partition.h triangulation.cpp triangulation.h regionMapping.cpp regionMapping.h -o run -IC:\codeProjects\nationGenerator\SFML-2.5.1\include -LC:\codeProjects\nationGenerator\SFML-2.5.1\lib -DSFML_STATIC -lsfml-graphics-s -lsfml-window-s -lsfml-system-s -lopengl32 -lfreetype -lwinmm -lgdi32
+
