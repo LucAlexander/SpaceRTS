1671ac32fb122bb71647d078970ed1a289331745
LucAlexander <46453661+LucAlexander@users.noreply.github.com>
2021-07-27 18:15:04 -0500

    fixed texture loading function
    back when data initialization occured in constructors, type references were setting off load functions, causing an error in code where the first time a texture was loaded it would not return the texture to not be caught, now since the first time textures are loaded into memory is for a real object, the function ahs been fixed to return the texture* it was meant to all along

--- e/textureTable.cpp
+++ e/textureTable.cpp
@@ -3,15 +3,15 @@
 std::map<const char*, sf::Texture*> txtab::index;
 
 sf::Texture* txtab::load(const char* dir){
-	if (index.count(dir)>0){
-		return index[dir];
+	if (index.count(dir)==0){
+		sf::Texture* tex = new sf::Texture();
+		std::string relativeDir(dir);
+		relativeDir = "./s/" + relativeDir;
+		tex->loadFromFile(relativeDir);
+		tex->setSmooth(false);
+		index[dir] = tex;
 	}
-	sf::Texture* tex = new sf::Texture();
-	std::string relativeDir(dir);
-	relativeDir = "./s/" + relativeDir;
-	tex->loadFromFile(relativeDir);
-	tex->setSmooth(false);
-	index[dir] = tex;
+	return index[dir];
 }
 
 void txtab::close(){
